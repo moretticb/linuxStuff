7c4e90fe2fda63f5528d1ad0422e5f5b3d8634e6
Caio Benatti Moretti <caiodba@gmail.com>
2018-07-30 09:12:29 -0300

    minor adjustments

--- mytools/url.sh
+++ mytools/url.sh
@@ -3,9 +3,10 @@
 # install dmenu
 # install sqlite
 # install xdg-utils
-# running with firefox only
+# compatible with firefox only
 
-url=$(sqlite3 /home/duzas/.mozilla/firefox/*.default/places.sqlite "select url,title from moz_places;" | grep http | sed -e "s/|/    /g" | dmenu -p "💩 URL (shift+enter)" -i -l 10 -nb "#EFEFEF" -sb "#333333" -nf "#000000" )
+#more about firefox sqlite tables at https://developer.mozilla.org/en-US/docs/Mozilla/Tech/Places/Database
+url=$(sqlite3 /home/duzas/.mozilla/firefox/*.default/places.sqlite "select moz_places.url,moz_places.title,moz_historyvisits.visit_date from moz_places,moz_historyvisits where moz_historyvisits.place_id=moz_places.id order by 3 desc;" | grep http | cut -d "|" -f 1-2 | sed -e "s/|/    /g" | dmenu -p "💩 URL (shift+enter)" -i -l 10 -nb "#EFEFEF" -sb "#333333" -nf "#000000" )
 
 if [ $? = 0 ]; then
 	if [ $(echo $url | cut -c1) = "_" ]; then
