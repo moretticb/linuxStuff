33f729127a5fbf112fdf884ae4753be72a50d87b
Caio Benatti Moretti <caiodba@gmail.com>
2018-07-31 19:14:07 -0300

    adding calc

---
+++ mytools/calc.sh
@@ -0,0 +1,24 @@
+xpr="0"
+ans="0"
+lastxpr="0"
+
+while :; do
+	xpr=$(echo | dmenu -p "ans = $ans" -sf "#FFFFFF" -sb "#000000" -nb "#FFFFFF" -nf "#000000" -fn "Ubuntu-25")
+
+	if [ $? = 1 ]; then
+		exit 1;
+	fi
+
+	if [ $xpr = "=" ]; then
+		xpr=$lastxpr
+	else
+		lastxpr=$xpr
+	fi
+
+	xpr=$(echo $xpr | sed $(echo "s/ans/$ans/g"))
+
+	echo "expressao: " $xpr
+
+	ans=$(echo $xpr | bc -l)
+
+done
--- mytools/pacmansearch.sh
+++ mytools/pacmansearch.sh
@@ -8,6 +8,6 @@ search=$(echo | dmenu -p "Pacman search:" -nf "#FFFF00" -nb "#000000" -sf "#0000
 if [ $? = 0 -a "$search" != "" ]; then
 	line=$(pacman -Ss $search | awk '{key=$0; getline; print key $0;}' | dmenu -l 10 -nf "#FFFF00" -nb "#000000" -sf "#000000" -sb "#FFFF00" -fn "Ubuntu-9")
 	if [ $? = 0 ]; then
-		termite --title="Installing $line" --exec="sudo pacman -S $(echo $line | cut -d " " -f 1)" --hold
+		termite --title="Installing $line" --exec="bash -c 'sudo pacman -S $(echo $line | cut -d " " -f 1); read xxx'"
 	fi
 fi
