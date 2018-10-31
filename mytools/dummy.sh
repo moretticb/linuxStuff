709a25d80617e8b677e4a74c3eda44e2ca77de50
Caio Benatti Moretti <caiodba@gmail.com>
2018-10-30 22:07:26 -0300

    enhancing handleaudio tool and fixing i3lock key binding

--- mytools/dummy.sh
+++ mytools/dummy.sh
@@ -1,4 +1,5 @@
 # demais programas para instalar
+# instalar pamixer
 # instalar firefox
 # instalar beep
 # instalar yaourt
@@ -6,4 +7,10 @@
 # instalar feh
 # instalar tcl
 # instalar R
+# instalar playerctl
+# instalar qt
+# instalar vlc
+
+# instalar google-drive-ocamlfuse
+
 
--- mytools/handleaudio.sh
+++ mytools/handleaudio.sh
@@ -1,4 +1,23 @@
-profile="$(pacmd list-cards | sed -E "s/^[[:space:]]+(input|output):/\1:/g" | grep ^[oi] | sed "s/: .*$//g" | dmenu -i -p "Audio card profile")"
+cards="$(pacmd list-cards | head -1 | cut -d' ' -f 1)"
+card="$(pacmd list-cards | grep alsa.card_name | cut -d'"' -f 2 | grep -n . | dmenu -i -p "Audio card" | cut -d':' -f 1)"
 
-echo changing to profile $profile
-pacmd set-card-profile 0 "$profile"
+cardIndex=$(echo "$card -1" | bc)
+
+if [ $cardIndex -lt 0 ]; then
+	exit 1
+fi
+
+profilesFirst=$(( $(pacmd list-cards | grep -n profiles | head -`echo $card` | tail -1 | cut -d':' -f 1)+1 ))
+profilesLast=$(( $(pacmd list-cards | grep -n active | head -`echo $card` | tail -1 | cut -d':' -f 1)-1 ))
+
+profile=$(pacmd list-cards | sed -n "$profilesFirst, $profilesLast p" | grep -v "available: no" | cut -d' ' -f 1 | sed "s/:$//g" | sed -E "s/^[[:space:]]+(.*)/\1/g" | dmenu -i -p "Card $card | Profile")
+
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+for i in `seq 0 $(( $cards-1 ))`; do
+	pacmd set-card-profile $i off
+done
+
+pacmd set-card-profile $cardIndex $profile
