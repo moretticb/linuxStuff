8c856b50436678ba81036bf4c386bf9d4b720eb5
Caio Benatti Moretti <caiodba@gmail.com>
2018-11-14 18:39:00 -0200

    adjusting i3 config files

--- install.sh
+++ install.sh
@@ -39,19 +39,4 @@ echo -e "\nxinitrc"
 ln -s -fi $HOME/linuxStuff/.xinitrc $HOME/.xinitrc
 
 
-echo "Install the following programs (requisites for the tools to work):"
-
-programs=$(cat mytools/* | grep instal | sort | uniq | cut -d" " -f 3)
-
-for tool in $programs; do
-	pacman -Ss $tool | grep "^[^[:space:]].*/$(echo $tool) " | grep -v installed
-done
-
-echo -e "\nInstall the following programs with yaourt (requisites for the tools to work):"
-
-programs=$(cat mytools/* | grep instal | sort | uniq | cut -d" " -f 3)
-
-for tool in $programs; do
-	yaourt -Ss $tool | grep "^[^[:space:]].*/$(echo $tool) " | grep -v installed
-done
-
+$HOME/linuxStuff/mytools/install_check.sh
---
+++ mytools/install_check.sh
@@ -0,0 +1,36 @@
+# demais programas para instalar
+# instalar pamixer
+# instalar xdotool
+# instalar firefox
+# instalar beep
+# instalar yaourt
+# instalar wget
+# instalar feh
+# instalar tcl
+# instalar R
+# instalar playerctl
+# instalar qt
+# instalar vlc
+# instalar evince
+# instalar jupyter
+# instalar transmission-cli
+
+
+# AUR
+# instalar google-drive-ocamlfuse
+# instalar otf-font-awesome-4
+
+programs=$(cat ./* | grep instal | sort | uniq | cut -d" " -f 3)
+
+for tool in $programs; do
+	pacman -Ss $tool | grep "^[^[:space:]].*/$(echo $tool) " | grep -v installed
+done
+
+echo -e "\nInstall the following programs with yaourt (requisites for the tools to work):"
+
+programs=$(cat ./* | grep instal | sort | uniq | cut -d" " -f 3)
+
+for tool in $programs; do
+	yaourt -Ss $tool | grep "^[^[:space:]].*/$(echo $tool) " | grep -v installed | grep aur
+done
+
