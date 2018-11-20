119d0b7a60688ac88f17769fc86f6f985c2c8c6d
Caio Benatti Moretti <caiodba@gmail.com>
2018-11-20 01:41:00 -0200

    adding delay to numlock listener tool

--- mytools/keylocks.sh
+++ mytools/keylocks.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sleep 0.05
+
 $HOME/linuxStuff/mytools/barmsg.sh "<small>$(xset q | grep Caps | sed "s/[0-9:]//g" | sed -E "s/(Caps|Num|Scroll) Lock[ ]+on/\n\1\n/g" | grep ^[A-Z] | tr "\n" " ")</small>"
--- mytools/yaourtsearch.sh
+++ mytools/yaourtsearch.sh
@@ -7,6 +7,6 @@ search=$(echo | dmenu -p "Yaourt search:" -nf "#FFA500" -nb "#000000" -sf "#0000
 if [ $? = 0 -a "$search" != "" ]; then
 	line=$(yaourt -Ss $search | awk '{key=$0; getline; print key $0;}' | grep aur | dmenu -l 10 -nf "#FFA500" -nb "#000000" -sf "#000000" -sb "#FFA500" -fn "Ubuntu-9")
 	if [ $? = 0 ]; then
-		termite --title="Installing $line" --exec="yaourt -S $(echo $line | cut -d " " -f 1)" --hold
+		termite --title="Installing $line" --exec="bash -c 'yaourt -S $(echo $line | cut -d " " -f 1) --noconfirm; read -p \"Yaourt installation finished\"'"
 	fi
 fi
