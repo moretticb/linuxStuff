0bbc58effb41ba41e814afa4af2427dae338509b
Caio Benatti Moretti <caiodba@gmail.com>
2018-12-11 16:25:21 -0200

    adding last language feature to vim man page loader script

--- mytools/vim_man.sh
+++ mytools/vim_man.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
-man $1
+clear
 
-if [ "$?" == "0" ]; then
-	exit 0;
+echo "> $1"
+echo -e "\nLANGUAGES:"
+echo "  0 - Google search"
+echo "  1 - Shell"
+echo "  2 - C"
+echo "  3 - R"
+
+echo -e "\nLast: $(cat /tmp/vim_lastdoc 2>/dev/null)"
+
+opt=0
+read -p "> " opt
+
+if [ "$opt" == "" ]; then
+	echo "using last option: $(cat /tmp/vim_lastdoc)"
+	opt=$(cat /tmp/vim_lastdoc)
 fi
 
-echo "?$1" | R --no-save
+echo $opt > /tmp/vim_lastdoc
+
+case $opt in
+	0) xdg-open "http://www.google.com/search?q=$1" ;;
+	1) man 1 $1 ;;
+	2) man 3 $1 ;;
+	3) echo "?$1" | R --no-save ;;
+	*) xdg-open "http://www.google.com/search?q=$1" ;;
+esac
+
+exit 0;
+
