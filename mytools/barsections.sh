b0642b0b355771462998fe4d5d354e73abbb0315
Caio Benatti Moretti <caiodba@gmail.com>
2022-03-26 06:25:45 -0300

    fixing representation of powerline symbols

--- mytools/barsections.sh
+++ mytools/barsections.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+L="&#57526;" #"\uE0B6"
+R="&#57524;" #"\uE0B4"
+
 if [ "$#" = 3 ]; then
-	echo -e "<span color='$1' bgalpha='40%' fgalpha='40%'>\uE0B6</span><span color='$2' bgcolor='$1' bgalpha='40%' fgalpha='40%'>$3</span><span color='$1' bgalpha='40%' fgalpha='40%'>\uE0B4</span>"
+	echo -e "<span color='$1' bgalpha='40%' fgalpha='40%'>$L</span><span color='$2' bgcolor='$1' bgalpha='40%' fgalpha='40%'>$3</span><span color='$1' bgalpha='40%' fgalpha='40%'>$R</span>"
 elif [ "$#" = 4 ]; then
-	echo -e "<span color='$1' bgalpha='40%' fgalpha='40%'>\uE0B6</span><span color='$2' bgcolor='$1' bgalpha='40%' fgalpha='40%'>$3</span><span color='$1' fgalpha='40%' bgcolor='$1' bgalpha='60%'>\uE0B4</span><span color='$2' bgcolor='$1' bgalpha='60%'> $4</span><span color='$1' fgalpha='60%'>\uE0B4</span>"
+	echo -e "<span color='$1' bgalpha='40%' fgalpha='40%'>$L</span><span color='$2' bgcolor='$1' bgalpha='40%' fgalpha='40%'>$3</span><span color='$1' fgalpha='40%' bgcolor='$1' bgalpha='60%'>$R</span><span color='$2' bgcolor='$1' bgalpha='60%'> $4</span><span color='$1' fgalpha='60%'>$R</span>"
 fi
