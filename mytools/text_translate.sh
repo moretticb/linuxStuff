2132cf1f0d4dbc8a56a7c4ef159cb576a61922e4
Caio Benatti Moretti <caiodba@gmail.com>
2022-03-26 01:47:11 -0300

    fixing translate tool for urlencoding input

--- mytools/text_translate.sh
+++ mytools/text_translate.sh
@@ -18,8 +18,18 @@ fi
 
 user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0"
 header="Accept: text/json"
-api_url="http://translate.googleapis.com/translate_a/single?client=gtx&sl=$from&tl=$to&dt=at&q=$1"
-line=$(curl --header "${header}" --user-agent "${user_agent}" -sL "${api_url}" | jq .[5][0][2][0][0] | dmenu -l 5 -p "(T $from-$to) $(ec    ho $1 | cut -b-15)..." -nb "#EFEFEF" -nf "#454545" -sb "#0288D1" -sf "#222222")
+#api_url="http://translate.googleapis.com/translate_a/single?client=gtx&sl=$from&tl=$to&dt=at&q=$1"
+api_url="http://translate.googleapis.com/translate_a/single"
+line=$(\
+	curl\
+	--header "${header}"\
+	--user-agent "${user_agent}"\
+	--data-urlencode "client=gtx"\
+	--data-urlencode "sl=$from"\
+	--data-urlencode "tl=$to"\
+	--data-urlencode "dt=at"\
+	--data-urlencode "q=$1"\
+	 -sL "${api_url}" | jq .[5][0][2][0][0] | dmenu -l 5 -p "(T $from-$to) $(echo $1 | cut -b-15)..." -nb "#EFEFEF" -nf "#454545" -sb "#0288D1" -sf "#222222")
 
 
 if [ "$?" = 1 ]; then
