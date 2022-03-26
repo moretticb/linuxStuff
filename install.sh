784dfdc05542ac4a920166c353db0cd9d7890f2f
Caio Benatti Moretti <caiodba@gmail.com>
2022-03-26 06:33:27 -0300

    removing interactivity parameter when linking dotfiles

--- install.sh
+++ install.sh
@@ -4,35 +4,32 @@ echo -e "\nCreating directory for i3blocks config file"
 mkdir -p $HOME/.config/i3blocks
 
 echo "Linking i3blocks config file"
-ln -s -fi $HOME/linuxStuff/.config/i3blocks/config $HOME/.config/i3blocks/config
+ln -s -f $HOME/linuxStuff/.config/i3blocks/config $HOME/.config/i3blocks/config
 
 
 echo -e "\nCreating directory for i3 config file"
 mkdir -p $HOME/.config/i3
 
 echo "Linking i3 config file"
-ln -s -fi $HOME/linuxStuff/.config/i3/config $HOME/.config/i3/config
+ln -s -f $HOME/linuxStuff/.config/i3/config $HOME/.config/i3/config
 
 echo -e "\nCreating directory for alacritty config file"
 mkdir -p $HOME/.config/alacritty
 
 echo "Linking alacritty config file"
-ln -s -fi $HOME/linuxStuff/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+ln -s -f $HOME/linuxStuff/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
 echo -e "\nLinking USPNet vpn connection script"
-sudo ln -s -fi $HOME/linuxStuff/mytools/vpn.sh /bin/vpn
-
-echo -e "\nLinking currency tool"
-sudo ln -s -fi $HOME/linuxStuff/mytools/currency.sh /bin/currency
+sudo ln -s -f $HOME/linuxStuff/mytools/vpn.sh /bin/vpn
 
 echo -e "\nLinking btdevices tool"
-sudo ln -s -fi $HOME/linuxStuff/mytools/btdevices.sh /bin/btdevices
+sudo ln -s -f $HOME/linuxStuff/mytools/btdevices.sh /bin/btdevices
 
 echo -e "\nLinking troubleshooting tool"
-sudo ln -s -fi $HOME/linuxStuff/mytools/troubleshooting.sh /bin/troubleshooting
+sudo ln -s -f $HOME/linuxStuff/mytools/troubleshooting.sh /bin/troubleshooting
 
 echo -e "\nLinking weather tool"
-sudo ln -s -fi $HOME/linuxStuff/mytools/weather.sh /bin/weather
+sudo ln -s -f $HOME/linuxStuff/mytools/weather.sh /bin/weather
 
 
 
@@ -44,23 +41,23 @@ mkdir -p $HOME/.screenshots
 echo -e "\nLinking config files"
 
 echo -e "\ngitconfig"
-ln -s -fi $HOME/linuxStuff/.gitconfig $HOME/.gitconfig
+ln -s -f $HOME/linuxStuff/.gitconfig $HOME/.gitconfig
 
 echo -e "\nvimrc"
-ln -s -fi $HOME/linuxStuff/.vimrc $HOME/.vimrc
+ln -s -f $HOME/linuxStuff/.vimrc $HOME/.vimrc
 
 echo -e "\npythonrc"
-ln -s -fi $HOME/linuxStuff/.pythonrc $HOME/.pythonrc
+ln -s -f $HOME/linuxStuff/.pythonrc $HOME/.pythonrc
 
 echo -e "\nbashrc"
-ln -s -fi $HOME/linuxStuff/.bashrc $HOME/.bashrc
+ln -s -f $HOME/linuxStuff/.bashrc $HOME/.bashrc
 
 echo -e "\nbash_profile"
-ln -s -fi $HOME/linuxStuff/.bash_profile $HOME/.bash_profile
+ln -s -f $HOME/linuxStuff/.bash_profile $HOME/.bash_profile
 
 echo -e "\nxinitrc"
-ln -s -fi $HOME/linuxStuff/.xinitrc $HOME/.xinitrc
+ln -s -f $HOME/linuxStuff/.xinitrc $HOME/.xinitrc
 
 echo -e "\nXmodmap"
-ln -s -fi $HOME/linuxStuff/.Xmodmap $HOME/.Xmodmap
+ln -s -f $HOME/linuxStuff/.Xmodmap $HOME/.Xmodmap
 
